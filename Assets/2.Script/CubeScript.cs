fd8010f131eb5ab50019d90565210e2925a6f0ae
BeomSeok-Seo <vlfksrl@naver.com>
2016-07-28 11:54:20 +0900

    Field_Card MoveAnimation Add

--- Assets/2.Script/CubeScript.cs
+++ Assets/2.Script/CubeScript.cs
@@ -1,11 +1,18 @@
-﻿using UnityEngine;
+﻿//작성자 : 서범석
+//목적 : 필드카드 이벤트 처리
+//최근 수정일 : 2016-07-28
+//수정 내용 
+// - 2016-07-14 : 스크립트 병합
+// - 2016-07-28 : 공격 애니메이션 제작
+using UnityEngine;
 using System.Collections;
 
 public class CubeScript : MonoBehaviour {
-	private Ray ray;
-	private RaycastHit hit;
+	
 
-	public int flag = 0;
+	public int flag = 0; // 공격용 플래그
+	private int movingFlag = 0;  // 움직임 애니메이션용 플래그
+	private int moveCount = 0; //움직임 애니메이션 흐름용 카운트
 
 	public int shock = 4;
 	public int resistance = 2;
@@ -21,6 +28,7 @@ public class CubeScript : MonoBehaviour {
 	private LineScript lines;
 
 	private Vector3 oldVector;
+	private Vector3 newVector;
 
 	// Use this for initialization
 	void Start () {
@@ -30,68 +38,31 @@ public class CubeScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-		if(Input.GetMouseButtonDown(0)){
-			ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-			if(Physics.Raycast(ray, out hit, Mathf.Infinity)){
-				if(hit.transform.gameObject == gameObject){
-					
-					//flag = 1;
-					//attack = hit.transform.gameObject.GetComponent<CubeScript>();
-					//Debug.Log (hit.transform.gameObject.name);
-					//fields.addBattleCard ();
-
-					//lines.setStartPosition (oldVector = hit.transform.position + new Vector3 (0, 10, 0));
-
-
-					FieldMouseDown (hit.transform.gameObject);
-				}
+		//움직이는 애니메이션
+		if (movingFlag == 1) {
+			this.gameObject.transform.Translate (
+				new Vector3 ((oldVector.x - newVector.x) / 10,
+					0,
+					(oldVector.z - newVector.z) / 10
+				)
+			);
+			moveCount++;
+			if (moveCount >= 10)
+				movingFlag = 2;
+		}
+		if (movingFlag == 2) {
+			this.gameObject.transform.Translate (
+				new Vector3 ((newVector.x - oldVector.x) / 10,
+					0,
+					(newVector.z - oldVector.z) / 10
+				)
+			);
+			moveCount--;
+			if (moveCount <= 0) {
+				movingFlag = 0;
+				CardToCard ();
 			}
 		}
-		if( Input.GetMouseButtonUp(0) ) 
-		{
-			if (flag == 1) {
-				ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-				//Debug.Log ("aa");
-
-				if (Physics.Raycast (ray, out hit, Mathf.Infinity)) {
-					//if (hit.transform.gameObject.tag == "Field_Card") {
-						
-					//	defense = hit.transform.gameObject.GetComponent<CubeScript> ();
-						//Debug.Log ("bb");
-
-					//	if (defense.flag == 0) {
-					//		defense.stamina = defense.stamina - attack.shock;
-					//		attack.stamina = attack.stamina - defense.resistance;
-					//		//Debug.Log ("cc");
-					//	}
-					//} else if (hit.transform.gameObject.tag == "Player") {
-
-					//}
-				
-					FieldMouseUp (hit.transform.gameObject);
-				}
-			}
-
-			lines.setStartPosition (new Vector3 (0, 10, 0));
-			lines.setLastPosition (new Vector3 (0, 10, 0));
-			flag = 0;
-		}
-
-
-		if (flag == 1) {
-			ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			lines.setLastPosition (oldVector);
-			if (Physics.Raycast (ray, out hit, Mathf.Infinity)) {
-				
-				//if(hit.transform.gameObject.tag == "Field_Card" || hit.transform.gameObject.tag == "Player")
-				//	lines.setLastPosition (hit.transform.position + new Vector3 (0, 10, 0));
-				
-				FieldUpdate (hit.transform.gameObject);
-			}
-		}
-
 	}
 
 	public void FieldMouseDown(GameObject gameObject) {
@@ -101,7 +72,8 @@ public class CubeScript : MonoBehaviour {
 		//Debug.Log (hit.transform.gameObject.name);
 		//fields.addBattleCard ();
 
-		lines.setStartPosition (oldVector = gameObject.transform.position + new Vector3 (0, 10, 0));
+		oldVector = gameObject.transform.position;
+		lines.setStartPosition (oldVector + new Vector3 (0, 10, 0));
 	}
 
 	public void FieldMouseUp(GameObject gameObject) {
@@ -112,9 +84,12 @@ public class CubeScript : MonoBehaviour {
 				deffenseObj = gameObject;
 				//Debug.Log ("bb");
 
+				newVector = gameObject.transform.position;
+
 				if (deffense.flag == 0) {
-					CardToCard ();
+					//CardToCard ();
 					//Debug.Log ("cc");
+					movingFlag = 1;
 				}
 			} else if (gameObject.transform.gameObject.tag == "Player") {
 				CardToPlayer ();
@@ -129,6 +104,7 @@ public class CubeScript : MonoBehaviour {
 
 	public void FieldUpdate(GameObject gameObject) {
 		if (flag == 1) {
+			//라인 그리기
 			lines.setLastPosition (oldVector);
 			if(gameObject.transform.gameObject.tag == "Field_Card" || gameObject.transform.gameObject.tag == "Player")
 				lines.setLastPosition (gameObject.transform.position + new Vector3 (0, 10, 0));
